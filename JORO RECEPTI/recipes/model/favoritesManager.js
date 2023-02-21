f4561cc8256149822a823cef9a9596f268deb385
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-02-21 09:20:54 +0200

    ALL FILES

---
+++ JORO RECEPTI/recipes/model/favoritesManager.js
@@ -0,0 +1,15 @@
+class favorites {
+    constructor (title,href,ingredients,thumbnail) {
+        this.title = title;
+        this.href = href;
+        this.ingredients = ingredients;
+        this.thumbnail = thumbnail;
+    }
+}
+
+class FavoriteManager {
+    constructor () {
+        this.favorites = []
+    }
+
+}
